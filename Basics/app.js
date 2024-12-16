9b3f67823c8022c79c21d39e8a42ad22dbd9d8a2
Gopal274 <gopaljha0877@gmail.com>
2024-12-16 16:52:40 +0530

    first commit

---
+++ Basics/app.js
@@ -0,0 +1,9 @@
+console.log("this is a warning");
+console.log("this is a warning");
+console.log("this is a warning");
+console.log("this is a warning");
+console.clear()
+console.warn("this is a warning");
+console.error("this is a warning");
+console.table({name:"huzain", age:20});
+console.log({name:"alex"})
