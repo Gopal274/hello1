86435eee575fcef196e3b45ef9b7bc641d6575bb
Gopal274 <gopaljha0877@gmail.com>
2024-12-16 17:42:39 +0530

    hello

--- Basics/app.js
+++ Basics/app.js
@@ -3,7 +3,63 @@ console.log("this is a warning");
 console.log("this is a warning");
 console.log("this is a warning");
 console.clear()
+
+
 console.warn("this is a warning");
 console.error("this is a warning");
 console.table({name:"huzain", age:20});
 console.log({name:"alex"})
+
+//comments
+//console.log("hello world")
+
+// variables
+
+//(reserved keyword) (variables) (types of value)
+
+//declare
+
+let name;
+console.log(name)
+
+let hello=5 ;
+
+console.log(hello)
+
+hello= 'gopal'
+console.log(hello)
+
+//assign value
+let banana;
+banana="banana"
+console.log(banana)
+
+//exercise
+let name1 = "gopal";
+let wanna =  "doctor";
+let gender =  "male";
+let twitterhandle=  "pata nahi"
+
+console.log(name1)
+console.log(wanna)
+console.log(gender)
+console.log(twitterhandle)
+
+ //primitive types
+    //1.Number
+    
+    let num = -10;
+    console.log(num)
+    
+    let num1 = -10.06;
+    console.log(num1)
+    
+    //basic math 
+    console.log(2+2)
+    console.log(2-2)
+    console.log(2*2)
+    console.log(2/2)
+    console.log(2%2)
+    console.log(22)
+    console.log(2**2)
+    
