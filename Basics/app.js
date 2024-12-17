f1036e52b6ee6c83caac99c0440052a89eff0c49
Gopal274 <gopaljha0877@gmail.com>
2024-12-17 13:17:45 +0530

    hell

--- Basics/app.js
+++ Basics/app.js
@@ -2,17 +2,17 @@ console.log("this is a warning");
 console.log("this is a warning");
 console.log("this is a warning");
 console.log("this is a warning");
-console.clear()
-
+console.clear();
 
 console.warn("this is a warning");
 console.error("this is a warning");
 console.table({
-  name: "huzain", age: 20
+  name: "huzain",
+  age: 20,
 });
 console.log({
-  name: "alex"
-})
+  name: "alex",
+});
 
 //comments
 //console.log("hello world")
@@ -24,50 +24,48 @@ console.log({
 //declare
 
 let name;
-console.log(name)
+console.log(name);
 
 let hello = 5;
 
-console.log(hello)
+console.log(hello);
 
-hello = 'gopal'
-console.log(hello)
+hello = "gopal";
+console.log(hello);
 
 //assign value
 let banana;
-banana = "banana"
-console.log(banana)
+banana = "banana";
+console.log(banana);
 
 //exercise
 let name1 = "gopal";
 let wanna = "doctor";
 let gender = "male";
-let twitterhandle = "pata nahi"
+let twitterhandle = "pata nahi";
 
-console.log(name1)
-console.log(wanna)
-console.log(gender)
-console.log(twitterhandle)
+console.log(name1);
+console.log(wanna);
+console.log(gender);
+console.log(twitterhandle);
 
 //primitive types
 //1.Number
 
 let num = -10;
-console.log(num)
+console.log(num);
 
 let num1 = -10.06;
-console.log(num1)
+console.log(num1);
 
 //basic math
-console.log(2+2)
-console.log(2-2)
-console.log(2*2)
-console.log(2/2)
-console.log(2%2)
-console.log(22)
-console.log(2**2)
-
-
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 / 2);
+console.log(2 % 2);
+console.log(22);
+console.log(2 ** 2);
 
 let counter = 0;
 counter++;
@@ -91,34 +89,32 @@ counter2--;
 
 console.log(counter2);
 
-
 //exercise
 
 let firstfavnumber = 13;
 let seconffavnumber = 17;
 
-console.log(firstfavnumber - seconffavnumber)
-console.log(firstfavnumber + seconffavnumber)
-console.log(firstfavnumber * seconffavnumber)
-console.log(firstfavnumber / seconffavnumber)
-console.log(firstfavnumber % seconffavnumber)
-console.log(firstfavnumber ** seconffavnumber)
-
+console.log(firstfavnumber - seconffavnumber);
+console.log(firstfavnumber + seconffavnumber);
+console.log(firstfavnumber * seconffavnumber);
+console.log(firstfavnumber / seconffavnumber);
+console.log(firstfavnumber % seconffavnumber);
+console.log(firstfavnumber ** seconffavnumber);
 
 //2.Boolean
 
 let istrue = true;
-console.log(istrue)
+console.log(istrue);
 
 let isfalse = false;
-console.log(isfalse)
+console.log(isfalse);
 
 let isNaN = NaN;
 
-console.log(isNaN)
+console.log(isNaN);
 
 let number = 10;
-console.log(number+undefined)
+console.log(number + undefined);
 
 //falsy-->
 
@@ -130,18 +126,28 @@ console.log(number+undefined)
 //NaN
 //empty--> '', "",
 
-
 let notdefined = undefined;
-console.log(notdefined)
+console.log(notdefined);
 
 let notdefined1 = null;
-console.log(notdefined1)
-
-
+console.log(notdefined1);
 
 //exercise
 
 let isJsProgramingLangauge = true;
 let isJsHard = false;
 let favNumb = 17;
-console.log(favNumb+undefined)
+console.log(favNumb + undefined);
+
+// comparison operator-->
+// 1.relational operator
+// 2.equality operator
+
+// 1.relational operator
+console.log(10 > 10);
+console.log(10 < 10);
+console.log(10 >= 10);
+console.log(10 <= 10);
+console.log(10 === 10);
+
+// 2.equality operator
