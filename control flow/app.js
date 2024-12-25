55039f6bbf4dbbd75b837769e78523263f45a515
Gopal274 <gopaljha0877@gmail.com>
2024-12-25 14:50:00 +0530

    update control flow/app.js

--- control flow/app.js
+++ control flow/app.js
@@ -54,28 +54,28 @@ switch (x) {
 }
 
 let day = "monday";
-switch(day){
-  case "monday":
-    console.log("today is monday");
-    break;
-  case "tuesday":
-    console.log("today is tuesday");
-    break;
-  case "wensday":
-    console.log("today is wensday");
-    break;
-  case "thursday":
-    console.log("today is thursday");
-    break;
-  case "friday":
-    console.log("today is friday");
-    break;
-  case "saturday":
-    console.log("today is saturday");
-    break;
-  case "sunday":
-    console.log("today is sunday");
-    break;
+switch (day) {
+    case "monday":
+        console.log("today is monday");
+        break;
+    case "tuesday":
+        console.log("today is tuesday");
+        break;
+    case "wensday":
+        console.log("today is wensday");
+        break;
+    case "thursday":
+        console.log("today is thursday");
+        break;
+    case "friday":
+        console.log("today is friday");
+        break;
+    case "saturday":
+        console.log("today is saturday");
+        break;
+    case "sunday":
+        console.log("today is sunday");
+        break;
     default:
-    console.log("dont know what day is today");
+        console.log("dont know what day is today");
 }
