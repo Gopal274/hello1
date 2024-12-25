437f4803b72d61eb7772cf60057308be5aea3db4
Gopal274 <gopaljha0877@gmail.com>
2024-12-25 14:39:19 +0530

    update control flow/app.js

--- control flow/app.js
+++ control flow/app.js
@@ -23,39 +23,32 @@ if (time < 10) {
     console.log("good after noon");
 }
 
-
-
-let password =5;
-if (password===8){
-  console.log("welcome")
-}
-else if(password<=8){
-  console.log("password is short")
-}
-else if(password>=8){
-  console.log("password is too long & password should have 8 character")
-}
-else{
-  console.log("please provide password")
+let password = 5;
+if (password === 8) {
+    console.log("welcome");
+} else if (password <= 8) {
+    console.log("password is short");
+} else if (password >= 8) {
+    console.log("password is too long & password should have 8 character");
+} else {
+    console.log("please provide password");
 }
 
 //switch statements
 
-let x = 1
+let x = 1;
 console.log;
 let bulb;
 switch (x) {
-  case 0:
-    bulb = 'off';
-    console.log(bulb)
-    break;
-  case 1: 
-    bulb = "on";
-    console.log(bulb)
-    break;
-  default:
-    bulb = "no velue found"
-    console.log(bulb)
-  
-  
+    case 0:
+        bulb = "off";
+        console.log(bulb);
+        break;
+    case 1:
+        bulb = "on";
+        console.log(bulb);
+        break;
+    default:
+        bulb = "no velue found";
+        console.log(bulb);
 }
