950775aeb55847345c09b1fa155f9e98f3676ba4
Gopal274 <gopaljha0877@gmail.com>
2024-12-25 14:49:58 +0530

    update control flow/app.js

--- control flow/app.js
+++ control flow/app.js
@@ -52,3 +52,30 @@ switch (x) {
         bulb = "no velue found";
         console.log(bulb);
 }
+
+let day = "monday";
+switch(day){
+  case "monday":
+    console.log("today is monday");
+    break;
+  case "tuesday":
+    console.log("today is tuesday");
+    break;
+  case "wensday":
+    console.log("today is wensday");
+    break;
+  case "thursday":
+    console.log("today is thursday");
+    break;
+  case "friday":
+    console.log("today is friday");
+    break;
+  case "saturday":
+    console.log("today is saturday");
+    break;
+  case "sunday":
+    console.log("today is sunday");
+    break;
+    default:
+    console.log("dont know what day is today");
+}
