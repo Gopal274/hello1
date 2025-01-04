655cb7db195f129ba13fc6b4dada43ba6301e6bf
Gopal274 <gopaljha0877@gmail.com>
2025-01-04 15:34:55 +0530

    update C++/basics/firstprogram.cpp

---
+++ C++/basics/firstprogram.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+init main(){
+  cout<<"hello world";
+  cout<<"hello world";
+  cout<<endl;//endl is used to print a new line
+  
+  cout<<"hello world"<<endl<<"hello world";
+  
+  cout<<endl;
+  cout<<"hello world ";
+  cout<<"/n";
+  cout<<"hello world"
+  
+  
+}
